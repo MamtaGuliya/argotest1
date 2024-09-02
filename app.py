76eab0d3fb938edb458c0e91118e640d96eb4028
MamtaGuliya <158456047+MamtaGuliya@users.noreply.github.com>
2024-09-02 13:56:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print("this is my first test")
+from flask import Flask
+
+app = Flask(_name_)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+if _name_ == '_main_':
+    app.run(host='0.0.0.0', port=8080)
